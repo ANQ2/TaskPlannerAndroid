75183d232d1712db1f74a145e43257b2ed30e992
deviannt <149984637+deviannt@users.noreply.github.com>
2024-12-17 21:27:29 +0500

    Update EditTaskScreen.kt

--- app/src/main/java/com/example/taskplanner/ui/theme/EditTaskScreen.kt
+++ app/src/main/java/com/example/taskplanner/ui/theme/EditTaskScreen.kt
@@ -1,4 +1,4 @@
-package com.example.taskplanner.ui 
+package com.example.taskplanner.ui  // Убедитесь, что это соответствует пути файла
 
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
