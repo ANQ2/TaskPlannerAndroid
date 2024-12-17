cfcf3e17d5f663ebea9e90797f186026d0abfc5a
deviannt <149984637+deviannt@users.noreply.github.com>
2024-12-17 16:15:21 +0500

    Update TaskRepository.kt

--- app/src/main/java/com/example/taskplanner/repository/TaskRepository.kt
+++ app/src/main/java/com/example/taskplanner/repository/TaskRepository.kt
@@ -7,15 +7,7 @@ import kotlinx.coroutines.flow.Flow
 class TaskRepository(private val taskDao: TaskDao) {
     val allTasks: Flow<List<Task>> = taskDao.getAllTasks()
 
-    suspend fun addTask(task: Task) {
-        taskDao.addTask(task)
-    }
-
-    suspend fun updateTask(task: Task) {
-        taskDao.updateTask(task)
-    }
-
-    suspend fun deleteTask(task: Task) {
-        taskDao.deleteTask(task)
-    }
+    suspend fun addTask(task: Task) = taskDao.addTask(task)
+    suspend fun updateTask(task: Task) = taskDao.updateTask(task)
+    suspend fun deleteTask(task: Task) = taskDao.deleteTask(task)
 }
